36d1aad7551e7115ebb9ae50678c823f6743e762
k2project <kopczkrzy@gmail.com>
2019-11-18 22:04:36 +0000

    project7

--- src/projects/Project7/Project7.js
+++ src/projects/Project7/Project7.js
@@ -1,37 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import './Project7.scss';
-import {useSpring, animated, useTrail,config } from 'react-spring';
+import { animated, useSprings,useSpring,config} from 'react-spring';
+import {Keyframes} from 'react-spring/renderprops'
+
 
 function Project7(){
-    const circles = [
-        '#F3F29A',
-        '#3DBADB',
-        '#70DFA6',
-        '#CE61A2',
+    const dots = [
+        {
+            color:'#F3F29A',
+            delay: 0,
+        },
+        {
+            color:'#3DBADB',
+            delay: 1000,
+        },
+        {
+            color:'#70DFA6',
+            delay:2000,
+        },
+        {
+            color:'#CE61A2',
+            delay:3000,
+        },
+
     ]
 
-    const trail = useTrail(circles.length, {
-            from:{top:'50px'},
-            to: async (next)=>{
-                await next ({top:'-50px'})
-                await next ({top:'0px'})
-            },
-            reset:true,
-    })
+    const springs = useSprings(dots.length, dots.map(item=>({
+        from:{
+            background:item.color,
+            top: '0px',
+        },
+        to:async (next) => {
+            await next({top:'50px'})
+            await next({top:'0px'})
+            await next({top:'50px'})
+            await next({top:'0px'})
+            await next({top:'-50px'})
+
+
+        },
+        config:{
+        },
+            // await next({ opacity: 1, from: { opacity: 0 }, reset: true })
+    })));
 
 
 
     return(
         <div className= "Project Project7" >
             <div>
-                {trail.map((props,i)=>{
-                    return(
-                        <animated.span key={'circle-p7-'+i} style={{
-                            ...props,
-                            background:circles[i],
-                        }} />
-                    )
-                })}
+                {springs.map(props=><animated.span style={props} />)}
+
             </div>
         </div>
     )
