591d7299098a4591b633b140bc6330999ac6bd62
k2project <kopczkrzy@gmail.com>
2019-11-20 09:43:22 +0000

    project7

--- src/projects/Project7/Project7.js
+++ src/projects/Project7/Project7.js
@@ -8,48 +8,50 @@ function Project7(){
     const dots = [
         {
             color:'#F3F29A',
-            delay: 0,
+        },
+        {
+            color:'#FF8A0C',
         },
         {
             color:'#3DBADB',
-            delay: 1000,
         },
         {
             color:'#70DFA6',
-            delay:2000,
         },
         {
             color:'#CE61A2',
-            delay:3000,
         },
 
     ]
-
-    const springs = useSprings(dots.length, dots.map(item=>({
+    const interp = i => r =>{
+        if(i% 2){
+            return  `translate3d(0, ${10 * Math.sin(r + ( 2 * Math.PI))}px, 0)`
+        }
+        return  `translate3d(0, ${5 * Math.sin(r + (3 * Math.PI))}px, 0)`
+    }
+    const {radians} = useSpring({
         from:{
-            background:item.color,
-            top: '0px',
+            radians:0,
         },
-        to:async (next) => {
-            await next({top:'50px'})
-            await next({top:'0px'})
-            await next({top:'50px'})
-            await next({top:'0px'})
-            await next({top:'-50px'})
+        to: async next => {
+          while (1) await next({ radians: 2 * Math.PI })
+        },
+        config: {
+            duration: 1000
+        },
+        reset: true,
 
-
-        },
-        config:{
-        },
-            // await next({ opacity: 1, from: { opacity: 0 }, reset: true })
-    })));
+    });
 
 
 
     return(
         <div className= "Project Project7" >
             <div>
-                {springs.map(props=><animated.span style={props} />)}
+                {dots.map((dot,i)=><animated.span key={'dot_'+i} style={{
+                    background:dots[i].color,
+                    transform: radians.interpolate(interp(i))
+                }} />)}
 
             </div>
         </div>
